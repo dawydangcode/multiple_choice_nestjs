c7c38e6096571efcdfe524e4ca9124f3556fc349
thach.bui <thach.bui@thenewtech.vn>
2025-06-10 16:15:50 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import { AppModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { throwError } from './utils/function';
 import { ConfigService } from '@nestjs/config';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
   const port = app.get(ConfigService).get<number>('app.port') ?? throwError();
   await app.listen(port);
 }
