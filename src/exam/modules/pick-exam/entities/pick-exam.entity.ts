3c2e1bfd6773cbf5b7b901e547f4a13815e7fc45
dawydangcode <giahuy260123@gmail.com>
2025-07-10 14:53:13 +0700

    feat(pick-exam): implement pick exam module with entity, DTO, model, controller, and service feat(topic): add topic module with entity, DTO, model, and necessary properties

---
+++ src/exam/modules/pick-exam/entities/pick-exam.entity.ts
@@ -0,0 +1,54 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { PickExamModel } from '../models/pick-exam.model';
+
+@Entity('pick_exam')
+export class PickExamEntity {
+  @PrimaryGeneratedColumn({ name: 'id' })
+  id!: number;
+
+  @Column({ name: 'exam_id' })
+  examId!: number;
+
+  @Column({ name: 'user_id' })
+  userId!: number;
+
+  @Column({ name: 'start_time' })
+  startTime!: Date;
+
+  @Column({ name: 'finish_time' })
+  finishTime!: Date;
+
+  @Column({ name: 'created_at' })
+  createdAt!: Date;
+
+  @Column({ name: 'created_by' })
+  createdBy!: number;
+
+  @Column({ name: 'updated_at' })
+  updatedAt!: Date;
+
+  @Column({ name: 'updated_by' })
+  updatedBy!: number;
+
+  @Column({ name: 'deleted_at' })
+  deletedAt!: Date;
+
+  @Column({ name: 'deleted_by' })
+  deletedBy!: number;
+
+  toModel(): PickExamModel {
+    return new PickExamModel(
+      this.id,
+      this.userId,
+      this.examId,
+      this.startTime,
+      this.finishTime,
+      this.createdAt,
+      this.createdBy,
+      this.updatedAt,
+      this.updatedBy,
+      this.deletedAt,
+      this.deletedBy,
+    );
+  }
+}
---
+++ src/topic/models/topic.model.ts
@@ -0,0 +1,33 @@
+export class TopicModel {
+  public readonly id: number;
+  public readonly name: string;
+  public readonly description: string | undefined;
+  public readonly createdAt: Date | undefined;
+  public readonly createdBy: number | undefined;
+  public readonly updatedAt: Date | undefined;
+  public readonly updatedBy: number | undefined;
+  public readonly deletedAt: Date | undefined;
+  public readonly deletedBy: number | undefined;
+
+  constructor(
+    id: number,
+    name: string,
+    description: string | undefined,
+    createdAt: Date | undefined,
+    createdBy: number | undefined,
+    updatedAt: Date | undefined,
+    updatedBy: number | undefined,
+    deletedAt: Date | undefined,
+    deletedBy: number | undefined,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.createdAt = createdAt;
+    this.createdBy = createdBy;
+    this.updatedAt = updatedAt;
+    this.updatedBy = updatedBy;
+    this.deletedAt = deletedAt;
+    this.deletedBy = deletedBy;
+  }
+}
