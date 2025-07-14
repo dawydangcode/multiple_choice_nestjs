213d2959733d56f41c1a4578c5d4408638f1eccc
dawydangcode <giahuy260123@gmail.com>
2025-07-14 11:41:48 +0700

    feat(topic): implement TopicDto and CRUD operations in TopicController and TopicService

--- src/topic/topic.controller.ts
+++ src/topic/topic.controller.ts
@@ -1,4 +1,68 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+} from '@nestjs/common';
+import { TopicService } from './topic.service';
+import {
+  CreateTopicBodyDto,
+  GetTopicParamsDto,
+  UpdateTopicBodyDto,
+} from './dtos/topic.dto';
+import { RequestModel } from 'src/utils/models/request.model';
 
-@Controller('api/v1/topic')
-export class TopicController {}
+@Controller('api/v1/')
+export class TopicController {
+  constructor(private readonly topicService: TopicService) {}
+
+  @Get('topic/list')
+  async getTopics() {
+    return await this.topicService.getTopics();
+  }
+
+  @Get('topic/:topicId/detail')
+  async getTopicById(@Param() params: GetTopicParamsDto) {
+    return await this.topicService.getTopicById(params.topicId);
+  }
+
+  @Post('topic/create')
+  async createTopic(
+    @Req() req: RequestModel,
+    @Body() body: CreateTopicBodyDto,
+  ) {
+    return await this.topicService.createTopic(
+      body.name,
+      body.description,
+      req.user.accountId,
+    );
+  }
+
+  @Put('topic/:topicId/update')
+  async updateTopic(
+    @Req() req: RequestModel,
+    @Param() params: GetTopicParamsDto,
+    @Body() body: UpdateTopicBodyDto,
+  ) {
+    const topic = await this.topicService.getTopicById(params.topicId);
+    return await this.topicService.updateTopic(
+      topic,
+      body.name,
+      body.description,
+      req.user.accountId,
+    );
+  }
+
+  @Delete('topic/:topicId/delete')
+  async deleteTopic(
+    @Req() req: RequestModel,
+    @Param() params: GetTopicParamsDto,
+  ) {
+    const topic = await this.topicService.getTopicById(params.topicId);
+    return await this.topicService.deleteTopic(topic, req.user.accountId);
+  }
+}
