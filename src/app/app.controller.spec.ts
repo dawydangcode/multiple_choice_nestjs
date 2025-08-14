c311b760eebbb4718e134085267d48010fba2907
dawydangcode <giahuy260123@gmail.com>
2025-08-14 15:54:45 +0700

    test: enhance AppController tests with additional providers and module configuration

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -7,6 +9,13 @@ describe('AppController', () => {
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
+      providers: [
+        AppService,
+        {
+          provide: ConfigService,
+          useValue: {}, // mock đơn giản, có thể bổ sung giá trị nếu cần
+        },
+      ],
     }).compile();
 
     appController = moduleRef.get<AppController>(AppController);
